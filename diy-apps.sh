2ea0c298e4040f7020c7b8d781f8deda6e4e8c90
caiwx86 <caiwenxiu0806@163.com>
2024-08-04 23:15:44 +0800

    add

--- diy-apps.sh
+++ diy-apps.sh
@@ -66,3 +66,5 @@ git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+
+# execute diy-script.sh

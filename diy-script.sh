b6ae09c4445e06d4a79fd0523adb4afe98b7e2bb
caiwx86 <caiwenxiu0806@163.com>
2024-08-16 21:31:54 +0800

    fix

--- diy-script.sh
+++ diy-script.sh
@@ -3,8 +3,8 @@
 # https://github.com/coolsnowwolf/lede/commits/master/include/kernel-5.15
 # git reset --hard 330337a64451fe229acdcaaab094677c627fb947 
 # 修改内核5.10
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
-sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
+# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
 
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.10.1/g' package/base-files/files/bin/config_generate

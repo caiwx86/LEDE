e0752bc2ac47ac29024986d0b50e59c92bb24660
caiwx86 <caiwenxiu0806@163.com>
2024-08-27 01:55:26 +0800

    clear

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -17,7 +17,6 @@ rm -rf feeds/luci/applications/luci-app-upnp # 网络唤醒
 rm -rf feeds/luci/applications/luci-app-wol # uu加速器
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-v2raya
-rm -rf feeds/packages/net/uugamebooster # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
 rm -rf feeds/packages/net/v2ray-geodata
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -34,7 +33,7 @@ function git_sparse_clone() {
 # AdGuardHome
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # syncthing turboacc uugame
-git_sparse_clone master  https://github.com/kiddin9/openwrt-packages luci-app-nps luci-app-docker luci-app-syncthing luci-app-turboacc uugamebooster
+git_sparse_clone master  https://github.com/kiddin9/openwrt-packages luci-app-nps luci-app-syncthing luci-app-turboacc
 
 # 科学上网插件
 # passwall passwall2 xray v2raya mosdns

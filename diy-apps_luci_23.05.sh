00865ded5df44933e3a82198f410f7e692086a4c
caiwx86 <caiwenxiu0806@163.com>
2024-09-08 04:35:18 +0800

    docker

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -35,7 +35,7 @@ function git_sparse_clone() {
 # syncthing turboacc uugame
 git_sparse_clone master  https://github.com/kiddin9/openwrt-packages \
   luci-app-adguardhome \
-  luci-app-dockerman \
+  luci-app-docker \
   luci-app-nps luci-app-syncthing luci-app-turboacc \
   luci-app-homeassistant luci-lib-taskd taskd luci-lib-xterm luci-app-homebridge
 

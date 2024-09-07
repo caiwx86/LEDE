9394827a40e64005e8c9de918157c6d1c522731e
caiwx86 <caiwenxiu0806@163.com>
2024-09-08 01:20:46 +0800

    fix

--- hotfix.sh
+++ hotfix.sh
@@ -3,7 +3,7 @@ echo "execute hotfix.sh"
 # https://github.com/coolsnowwolf/lede/commits/master/include/kernel-5.15
 # git reset --hard 330337a64451fe229acdcaaab094677c627fb947 
 # 修改内核5.10
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
+# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
 
 # 修复rockchip build error

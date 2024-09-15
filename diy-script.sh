0a2ba926963e7b1f6a9a16367acea85bbc780fad
caiwx86 <caiwenxiu0806@163.com>
2024-09-16 05:31:02 +0800

    fix adguardhome workdir

--- diy-script.sh
+++ diy-script.sh
@@ -15,7 +15,7 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 #修改luci-app-adguardhome配置config文件
 mkdir -p files/usr/bin
 cp $GITHUB_WORKSPACE/scripts/adguard_update_dhcp_leases.sh files/usr/bin/adguard_update_dhcp_leases.sh
-sed -i "s|option workdir '/usr/bin/AdGuardHome'|option workdir '/opt/AdGuardHome'|" package/luci-app-adguardhome/root/etc/config/AdGuardHome
+sed -i "s|option workdir '/etc/AdGuardHome'|option workdir '/opt/AdGuardHome'|" package/luci-app-adguardhome/root/etc/config/AdGuardHome
 sed -i "s|option configpath '/etc/AdGuardHome.yaml'|option configpath '/opt/AdGuardHome/AdGuardHome.yaml'|" package/luci-app-adguardhome/root/etc/config/AdGuardHome
 
 # 更改 Argon 主题背景

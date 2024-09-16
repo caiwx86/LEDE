7df20e38fe49a13135bde801b16c4d5709d8eadc
caiwx86 <caiwenxiu0806@163.com>
2024-09-17 02:20:11 +0800

    clean

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -2,23 +2,11 @@
 echo "execute diy-apps_luci_23.05.sh"
 ##  ======该脚本主要是拉取Apps============
 # 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/packages/net/{mosdns,msd_lite,smartdns,v2ray-geodata,adguardhome}
+rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear,luci-app-mosdns,luci-app-serverchan}
 # 移除不需要的包
-rm -rf feeds/luci/applications/luci-app-vlmcsd # 上网时间控制
-rm -rf feeds/luci/applications/luci-app-accesscontrol # DDNS
-rm -rf feeds/luci/applications/luci-app-ddns # UPNP
-rm -rf feeds/luci/applications/luci-app-upnp # 网络唤醒
-rm -rf feeds/luci/applications/luci-app-wol # uu加速器
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/applications/luci-app-v2raya
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/luci/applications/{luci-app-vlmcsd,luci-app-accesscontrol,luci-app-ddns,luci-app-upnp,luci-app-wol}
+rm -rf feeds/luci/applications/{luci-app-smartdns,luci-app-v2raya}
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -31,10 +19,9 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
-# AdGuardHome
-# git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-# syncthing turboacc uugame
 git_sparse_clone master  https://github.com/kiddin9/openwrt-packages \
+  luci-theme-argon luci-app-argon-config \
+  smartdns luci-app-smartdns \
   luci-app-docker \
   adguardhome luci-app-adguardhome \
   luci-app-nps luci-app-syncthing luci-app-turboacc
@@ -44,19 +31,3 @@ git_sparse_clone master  https://github.com/kiddin9/openwrt-packages \
 # 科学上网插件
 # passwall passwall2 xray v2raya mosdns
 git clone --depth=1 -b master https://github.com/kenzok8/small package/luci-app-passwall
-
-# SmartDNS
-git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 -b master https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# Themes
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
-# 在线用户
-#git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
-#sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-#sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-#chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-
-# execute diy-script.sh

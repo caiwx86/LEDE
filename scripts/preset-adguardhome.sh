0566e23c1659ef01b1b0e2b52a047824659800df
caiwx86 <caiwenxiu0806@163.com>
2024-09-17 14:07:22 +0800

    fix

--- scripts/preset-adguardhome.sh
+++ scripts/preset-adguardhome.sh
@@ -2,7 +2,12 @@
 echo "execute preset-adguardhome.sh"
 #修改luci-app-adguardhome配置config文件
 cd $OPENWRT_PATH
-mkdir -p files/usr/bin
+# mkdir -p files/usr/bin
+mkdir -p files/usr/bin/AdGuardHome
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+
 cp $GITHUB_WORKSPACE/scripts/adguard_update_dhcp_leases.sh files/usr/bin/adguard_update_dhcp_leases.sh
 sed -i "s|option workdir '/etc/AdGuardHome'|option workdir '/opt/AdGuardHome'|" package/luci-app-adguardhome/root/etc/config/AdGuardHome
 sed -i "s|option configpath '/etc/AdGuardHome.yaml'|option configpath '/opt/AdGuardHome/AdGuardHome.yaml'|" package/luci-app-adguardhome/root/etc/config/AdGuardHome

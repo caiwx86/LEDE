beda0a94f1a89160f251845721666c9e5dcdde9a
caiwx86 <caiwenxiu0806@163.com>
2024-11-26 21:58:46 +0800

    Update hotfix.sh

--- hotfix.sh
+++ hotfix.sh
@@ -6,7 +6,7 @@ echo "execute hotfix.sh"
 # 修改内核5.10
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
-rm -rf feeds/packages/lang/php7
+# rm -rf feeds/packages/lang/php7
 # sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
 
 # 修复rockchip build error

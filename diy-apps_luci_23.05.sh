f53fef1ac59985ff9ae33ea518747b898ad878a5
caiwx86 <caiwenxiu0806@163.com>
2024-11-26 21:58:28 +0800

    Update diy-apps_luci_23.05.sh

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -4,7 +4,7 @@ echo "execute diy-apps_luci_23.05.sh"
 # 移除不需要的包
 rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear}
 rm -rf feeds/packages/net/{mosdns,smartdns,v2ray-geodata}
-rm -rf feeds/luci/applications/{luci-app-vlmcsd,luci-app-accesscontrol,luci-app-ddns,luci-app-wol}
+rm -rf feeds/luci/applications/{luci-app-vlmcsd,luci-app-accesscontrol,luci-app-ddns,luci-app-wol,luci-app-kodexplorer}
 rm -rf feeds/luci/applications/{luci-app-smartdns,luci-app-v2raya,luci-app-mosdns,luci-app-serverchan}
 
 # Git稀疏克隆，只克隆指定目录到本地

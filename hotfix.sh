61a4463180b2a64374c09377cdc52d1eb1dc8f1b
caiwx86 <caiwenxiu0806@163.com>
2024-12-07 18:32:49 +0800

    Update hotfix.sh

--- hotfix.sh
+++ hotfix.sh
@@ -5,7 +5,7 @@ echo "execute hotfix.sh"
 # git reset --hard 330337a64451fe229acdcaaab094677c627fb947 
 # 修改内核5.10
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 # rm -rf feeds/packages/lang/php7
 # sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
 

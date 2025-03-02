3902c5d52b794dececd9a0c0cec60a2a7116c337
caiwx86 <caiwenxiu0806@163.com>
2025-03-02 11:36:10 +0800

    fix

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -31,7 +31,12 @@ git_sparse_clone small-package  https://github.com/caiwx86/openwrt-packages \
 # 科学上网插件
 # passwall passwall2 xray v2raya mosdns
 # git_sparse_clone main https://github.com/caiwx86/openwrt-packages small
-git clone https://github.com/kenzok8/small package/small
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 在线用户
 git_sparse_clone main https://github.com/danchexiaoyang/luci-app-onliner luci-app-onliner 

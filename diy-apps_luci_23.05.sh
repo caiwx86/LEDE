81136839858f0e478f6c511bf9a69eddf04fd413
caiwx86 <caiwenxiu0806@163.com>
2025-03-08 12:06:37 +0800

    fix

--- diy-apps_luci_23.05.sh
+++ diy-apps_luci_23.05.sh
@@ -30,7 +30,7 @@ git_sparse_clone small-package  https://github.com/caiwx86/openwrt-packages \
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone --depth=1  https://github.com/kenzok8/openwrt-packages package/kenzok8
 git clone --depth=1  https://github.com/kenzok8/small package/small
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns,geoview}
 rm -rf feeds/packages/utils/v2dat
 
 # 在线用户

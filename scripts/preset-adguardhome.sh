345e1002742c207adf6e6fec3adc4187e2d3c9d8
caiwx86 <caiwenxiu0806@163.com>
2025-03-13 23:52:22 +0800

    fix

--- scripts/preset-adguardhome.sh
+++ scripts/preset-adguardhome.sh
@@ -8,7 +8,8 @@ adguard_conf=feeds/luci/applications/luci-app-adguardhome/root/etc/config/AdGuar
 # 替换AdguardHome默认配置文件路径
 cp $GITHUB_WORKSPACE/scripts/adguard_update_dhcp_leases.sh files/usr/bin/adguard_update_dhcp_leases.sh
 sed -i "s|binpath.*|binpath '/usr/bin/AdGuardHome/AdGuardHome'|g" $adguard_conf
-# sed -i "s|option workdir '/etc/AdGuardHome'|option workdir '/opt/AdGuardHome'|" $adguard_conf
+sed -i "s|workdir.*|workdir '/usr/bin/AdGuardHome'|g" $adguard_conf
+#sed -i "s|option workdir '/etc/AdGuardHome'|option workdir '/opt/AdGuardHome'|" $adguard_conf
 # sed -i "s|option configpath '/etc/AdGuardHome.yaml'|option configpath '/opt/AdGuardHome/AdGuardHome.yaml'|" $adguard_conf
 
 # 更新为AdguardHome最新版本

1f2c8ec828f17fe5ddcfbcda87f3fa18b804a49a
caiwx86 <caiwenxiu0806@163.com>
2025-03-15 17:03:19 +0800

    fix

--- hotfix.sh
+++ hotfix.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 echo "execute hotfix.sh"
-# 临时调整文件
-# https://github.com/coolsnowwolf/lede/commits/master/include/kernel-5.15
-# git reset --hard 330337a64451fe229acdcaaab094677c627fb947 
-# 修改内核5.10
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
-# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
-# rm -rf feeds/packages/lang/php7
-# sed -i 's/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
-
-# 修复rockchip build error
-# sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
-
-# 调整 Docker 到 服务 菜单
-# sed -i 's/"admin"/"dmin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
-# sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
-# sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
-# sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
+# 修改内核版本
+# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/rockchip/Makefile
 
 # luci23.05
 # 调整 ttyd 到 系统 菜单
@@ -36,9 +21,7 @@ sed -i 's/services/network/g' $(find ./feeds/luci/applications/luci-app-upnp/roo
 sed -i 's/services/nas/g' $(find ./feeds/luci/applications/luci-app-alist/root/usr/share/luci/menu.d/ -type f -name "luci-app-alist.json")
 sed -i 's/services/nas/g' $(find ./feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/ -type f -name "luci-app-samba4.json")
 
-
 PKG_PATH="$OPENWRT_PATH/package/"
-
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
 	HP_RULE="surge"

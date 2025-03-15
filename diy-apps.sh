95493d9a9a28a2ec3102f5701e2eef8dd8502e42
caiwx86 <caiwenxiu0806@163.com>
2025-03-15 16:47:09 +0800

    fix

--- diy-apps.sh
+++ diy-apps.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-echo "execute diy-apps.sh"
+echo "execute diy-apps_luci_23.05.sh"
 ##  ======该脚本主要是拉取Apps============
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
 # 移除不需要的包
-rm -rf feeds/luci/applications/luci-app-vlmcsd # 上网时间控制
-rm -rf feeds/luci/applications/luci-app-accesscontrol # DDNS
-rm -rf feeds/luci/applications/luci-app-ddns # UPNP
-rm -rf feeds/luci/applications/luci-app-upnp # 网络唤醒
-rm -rf feeds/luci/applications/luci-app-wol # uu加速器
-rm -rf feeds/packages/net/uugamebooster # remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear}
+rm -rf feeds/packages/net/{mosdns,smartdns,v2ray-geodata}
+rm -rf feeds/luci/applications/{luci-app-vlmcsd,luci-app-accesscontrol,luci-app-ddns,luci-app-wol,luci-app-kodexplorer}
+rm -rf feeds/luci/applications/{luci-app-smartdns,luci-app-v2raya,luci-app-mosdns,luci-app-serverchan,luci-app-passwall2}
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -29,43 +17,37 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# v2raya 
-git clone --depth=1 -b 18.06 https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
-# xray
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-syncthing luci-app-bypass uugamebooster v2raya xray-core hysteria
-mkdir -p files/usr/share/xray
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat  -O files/usr/share/xray/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O files/usr/share/xray/geosite.dat
-
 # 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
-git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+git_sparse_clone small-package  https://github.com/caiwx86/openwrt-packages \
+  luci-app-npc luci-app-syncthing
+#  luci-app-homeassistant luci-lib-taskd taskd luci-lib-xterm
+#   luci-app-homebridge
 
 # 科学上网插件
-# git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-
-# SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# MosDNS
-git clone --depth=1 -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# Themes
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# passwall2 xray v2raya mosdns luci-app-ssr-plus luci-app-amlogic luci-app-smartdns luci-theme-argon
+# git_sparse_clone main https://github.com/caiwx86/openwrt-packages small
+git clone --depth=1  https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone --depth=1  https://github.com/kenzok8/small package/small
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns,geoview}
+rm -rf feeds/packages/utils/v2dat
 
 # 在线用户
-git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+git_sparse_clone main https://github.com/danchexiaoyang/luci-app-onliner luci-app-onliner 
 
-# execute diy-script.sh
+if [[ $AMLOGIC == "true" ]]; then
+# 晶晨宝盒
+# git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
+sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/caiwx86/OpenWrt'|g" package/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|shared_fstype.*|shared_fstype 'btrfs'|g" package/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
+echo CONFIG_PACKAGE_luci-app-amlogic=y >>  $OPENWRT_PATH/.config
+fi
+
+# adguardhome
+bash $GITHUB_WORKSPACE/scripts/preset-adguardhome.sh
+
+chmod +x $GITHUB_WORKSPACE/scripts/qca/*.sh
+# $GITHUB_WORKSPACE/scripts/qca/Packages.sh
+$GITHUB_WORKSPACE/scripts/qca/Handles.sh
+$GITHUB_WORKSPACE/scripts/qca/Settings.sh

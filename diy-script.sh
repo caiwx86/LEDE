362a2554fa8dbd68084ea72daf3050eadd26a27e
caiwx86 <caiwenxiu0806@163.com>
2025-04-07 11:13:20 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -13,7 +13,7 @@ shopt -s globstar
 chmod +x $GITHUB_WORKSPACE/scripts/function.sh && $GITHUB_WORKSPACE/scripts/function.sh
 
 # 修改内核版本
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/qualcommax/Makefile 
+# sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.6/g' target/linux/qualcommax/Makefile 
 
 update_feeds() {
     FEEDS_CONF="$OPENWRT_PATH/feeds.conf.default"
